e3dd6dddd65804480b875d96e698c848c0bbfd0f
James Quick <james.q.quick@outlook.com>
2015-12-29 10:10:28 -0500

    Initial Commit

---
+++ StarWars-ListView/StarWars-ListView/StarWars_ListView.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace StarWars_ListView.Droid
+{
+	[Activity (Label = "StarWars_ListView", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new StarWars_ListView.App ());
+		}
+	}
+}
+
---
+++ StarWars-ListView/StarWars-ListView/StarWars_ListView/Home.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace StarWars_ListView
+{
+	public class Home : ContentPage
+	{
+		Label chosenLabel;
+        public Home ()
+		{
+			string[] characterNames = new string[] { "C-3PO", "Chewbacca", "Darth Vader", "Luke SkyWalker",
+			"Princess Leia","R2D2", "Yoda" };
+
+			Label title = new Label
+			{
+				Text = "Star Wars ListView",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.StartAndExpand,
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+			};
+
+			ListView list = new ListView();
+			list.ItemsSource = characterNames;
+			list.ItemTapped += List_ItemTapped;
+
+			chosenLabel = new Label
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.StartAndExpand,
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+			};
+			StackLayout stack = new StackLayout
+			{
+				Children = {title,list, chosenLabel }
+			};
+			this.Content = stack;
+			this.Padding = new Thickness(10, Device.OnPlatform(20, 10, 10), 10, 10);
+		}
+
+		private void List_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			chosenLabel.Text = "You Selected " + e.Item;
+		}
+	}
+}
